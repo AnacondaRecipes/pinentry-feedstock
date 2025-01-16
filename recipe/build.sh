75122978b3f9d8847d7058663079d1c1aea0f59b
Andrii Osipov <aosipov@anaconda.com>
2025-01-16 10:48:55 -0800

    Fix ar attempt 1

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,11 @@ set -e -o pipefail -x
 
 cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./build-aux
 
+# make[2]: ar: Command not found (linux-aarch64 platform only!)
+if [[ "${target_platform}" == "linux-aarch64" ]]; then
+    export AR=$(basename $AR)
+fi
+
 ./autogen.sh
 ./configure --enable-pinentry-tty --disable-pinentry-curses --disable-pinentry-qt --disable-pinentry-gtk2 --disable-doc --prefix=${PREFIX}
 make
