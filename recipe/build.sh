75a56d9b13c3a989f25ff2f7c00163d54fcbfea8
Andrii Osipov <aosipov@anaconda.com>
2025-01-16 10:26:33 -0800

    Removed ncurses

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -e -o pipefail -x
 cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./build-aux
 
 ./autogen.sh
-./configure --disable-pinentry-qt --disable-pinentry-gtk2 --disable-doc --prefix=${PREFIX}
+./configure --enable-pinentry-tty --disable-pinentry-curses --disable-pinentry-qt --disable-pinentry-gtk2 --disable-doc --prefix=${PREFIX}
 make
 make install
 
