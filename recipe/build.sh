e0fa89d7fc93bbcc5b51d99c9b2d7dbfc6aa4cc1
Andrii-Osipov <aosipov@anaconda.com>
2025-01-17 08:18:44 -0800

    PKG-6749: pinentry 1.3.1 (#1)
    * PKG-6749: pinentry 1.3.1

---
+++ recipe/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e -o pipefail -x
+
+cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./build-aux
+
+# make[2]: ar: Command not found (linux-aarch64 platform only!)
+if [ $(uname -s) = "Linux" ] && [ ! -f "${BUILD_PREFIX}/bin/ar" ]; then
+    ln -s "${AR}" "${BUILD_PREFIX}/bin/ar"
+    ln -s "$RANLIB" "${BUILD_PREFIX}/bin/ranlib"
+    ln -sf "$LD" "${BUILD_PREFIX}/bin/ld"
+fi
+
+./autogen.sh
+./configure --enable-pinentry-tty --disable-pinentry-curses --disable-pinentry-qt --disable-pinentry-gtk2 --disable-doc --prefix=${PREFIX}
+make
+make install
+
