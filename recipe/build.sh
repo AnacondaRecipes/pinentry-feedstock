b6acbd7be805c5fa537126e95f670e322b4d072c
Andrii Osipov <aosipov@anaconda.com>
2025-01-17 18:01:39 -0800

    Changed AR and added helpcommand

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./build-aux
 
 # make[2]: ar: Command not found (linux-aarch64 platform only!)
 if [ $(uname -s) = "Linux" ] && [ ! -f "${BUILD_PREFIX}/bin/ar" ]; then
-    ln -s "${BUILD}-ar" "${BUILD_PREFIX}/bin/ar"
+    ln -s "${AR}" "${BUILD_PREFIX}/bin/ar"
     ln -s "$RANLIB" "${BUILD_PREFIX}/bin/ranlib"
     ln -sf "$LD" "${BUILD_PREFIX}/bin/ld"
 fi
